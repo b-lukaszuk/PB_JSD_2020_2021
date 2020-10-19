871353b19e1b4beb3398153e18e296c8f8d5d7e4
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2020-10-19 20:55:49 +0200

    dodano rozwiazania zadan 6-9 + kosmetyka

--- s1z1_17_10_2020/pd1.js
+++ s1z1_17_10_2020/pd1.js
@@ -26,7 +26,8 @@ function czyPrzestepny(rok) {
     }
 }
 
-// jakis bug w VS Code, console.log() nie drukuje prawidlowo Array-sow w Debug Console
+// jakis bug w VS Code, console.log() 
+// nie drukuje prawidlowo Array-sow w Debug Console
 // tzn. 1x drukuje ok, ale potem juz nie
 console.log(lata.map(czyPrzestepny).toString());
 
@@ -47,7 +48,7 @@ function silniaFor(liczba = 10) {
 
 
 /**
- * Liczy silnie (factorial) dla danej liczby rekurs. (taka jest def matem.)
+ * Liczy silnie (factorial) dla danej liczby rekurs. (taka jest def mat.)
  * (rekursja - nie dawac duzej liczby wejsciowej)
  * @param {Number} liczba (Int, >= 1), 
  * @return {Number} silnia (Int, >= 1)
@@ -60,8 +61,8 @@ function silniaRek(liczba = 10) {
     }
 }
 
-console.log(silniaFor(5));
-console.log(silniaRek(6));
+console.log(silniaFor(7));
+console.log(silniaRek(7));
 
 // 3. Calculate the sum of the odd items [1,6,23,8,4,98,3,7,3,98,4,98]
 let cyfry = [1, 6, 23, 8, 4, 98, 3, 7, 3, 98, 4, 98];
@@ -94,9 +95,8 @@ console.log(sumujNparz(cyfry));
 
 // 4. Choose highest and lowest values from the given array.
 // [1,6,23,8,4,98,3,7,3,98,4,98]. 
-// One loop run. (chodzi chyba o uzycie for-a 1x, a nie o 1 iteracje/obrot for-a)
-
-let cyfry2 = [1, 6, 23, 8, 4, 98, 3, 7, 3, 98, 4, 98];
+// One loop run.
+// (chodzi chyba o uzycie for-a 1x, a nie o 1 iteracje/obrot for-a)
 
 /** 
 * zwraca min i max z tabeli uzywajac for-a tylko 1x
@@ -118,17 +118,21 @@ function zwrocMinAndMax(liczby) {
     return [min, max];
 }
 
-// jakis bug w VS Code, console.log() nie drukuje prawidlowo Array-sow w Debug Console
-console.log(zwrocMinAndMax(cyfry2).toString());
+// jakis bug w VS Code, console.log()
+// nie drukuje prawidlowo Array-sow w Debug Console
+console.log(zwrocMinAndMax(cyfry).toString());
 
-// 5. Choose longest string from the array. [‘Karol’, ‘Adam’,’Rogowski’,’Politechnika’,’Super’,’Weekend’]. 
+// 5. Choose longest string from the array.
+// [‘Karol’, ‘Adam’,’Rogowski’,’Politechnika’,’Super’,’Weekend’]. 
 // tylko 1, czy kilka w przypadku remisow?! (wezme kilka)
-let tabStringow = ["Karol", "Adam", "Rogowski", "Politechnika", "Super", "Weekend"];
+let tabStringow = ["Karol", "Adam", "Rogowski",
+    "Politechnika", "Super", "Weekend"];
 
 /** 
-* Zwraca tablice z najdluzszym stringiem/ami lub [] jesli input to []
+* Zwraca tablice z najdluzszym stringiem/ami lub [] (jesli input to [])
 * @param {Array<String>} tabStringow - tablica stringow
-* @return {Array<String>} tabNajdlStringow - tablica najdluzszych stringow (String-i) lub []
+* @return {Array<String>} tabNajdlStringow - tablica najdluzszych stringow
+* (String-i) lub []
 */
 function zwrocNajdlStringi(tabStringow) {
     // przy pustej tablicy tabDlStringow to []
@@ -140,16 +144,86 @@ function zwrocNajdlStringi(tabStringow) {
     return tabStringow.filter((s) => s.length === dlMax);
 }
 
-// jakis bug w VS Code, console.log() nie drukuje prawidlowo Array-sow w Debug Console
+// jakis bug w VS Code, console.log()
+// nie drukuje prawidlowo Array-sow w Debug Console
 console.log(zwrocNajdlStringi(tabStringow).toString());
 
-// 6. Choose all the indexes on the highest value from the given array. [1,6,23,8,4,98,3,7,3,98,4,98].
-let cyfry3 = [1, 6, 23, 8, 4, 98, 3, 7, 3, 98, 4, 98];
+// 6. Choose all the indexes on the highest value from the given array.
+// [1,6,23,8,4,98,3,7,3,98,4,98].
+
+/** 
+* zwraca tabele z indeksem/ami max-a/ow w danym array-u (input)
+* @param {Array<Number>} liczby - tablica liczb
+* @return {Array<Number>} tabIndxyMaxow - indeksy max-a/ow w tablicy liczby
+*/
+function zwrocIndxyMaxow(liczby) {
+    let maxZtab = Math.max(...liczby);
+    let tabIndxyMaxow = [];
+    for (let i = 0; i < liczby.length; i++) {
+        if (liczby[i] === maxZtab) {
+            tabIndxyMaxow.push(i);
+        }
+    }
+    return tabIndxyMaxow;
+}
+
+// jakis bug w VS Code, console.log()
+// nie drukuje prawidlowo Array-sow w Debug Console
+console.log(zwrocIndxyMaxow(cyfry).toString());
 
 
+// 7. Calculate average value from the given array for even numbers
+// [1,6,23,8,4,98,3,7,3,98,4,98]
 
-// 7. Calculate average value from the given array for even numbers [1,6,23,8,4,98,3,7,3,98,4,98]
+/** 
+* liczy srednia z liczb parzystych w tabeli
+* @param {Array<Number>} liczby - tabela liczb
+* @return {Number} srednia z liczb parzystych z tabeli
+*/
+function liczSrParzystych(liczby) {
+    let tabParzyste = liczby.filter((a) => a % 2 === 0);
+    let sumaParz = tabParzyste.reduce((a, b) => a + b, 0);
+    return sumaParz / tabParzyste.length;
+}
 
-// 8. Calculate average value of items at even indexes. Zero is not considered to be even number. [1,6,23,8,4,98,3,7,3,98,4,98]
+console.log(liczSrParzystych(cyfry));
 
-// 9. With a given start value of 0. Iterate the array and add even items and subtract odd ones. [1,6,23,8,4,98,3,7,3,98,4,98]
+// 8. Calculate average value of items at even indexes.
+// Zero is not considered to be even number. [1,6,23,8,4,98,3,7,3,98,4,98]
+
+/** 
+* liczy srednia liczb z tablicy, dla liczb na parzystych indeksach (> 1)
+* @param {Array<Number>} liczby - tablica liczb
+* @return {Number} srednia liczb z indeksow parzystych powyzej 1
+*/
+function liczSrNaParzInd(liczby) {
+    let tabLiczbZparzInd = [];
+    for (let i = 2; i < liczby.length; i++) {
+        if (i % 2 === 0) {
+            tabLiczbZparzInd.push(liczby[i]);
+        }
+    }
+    let suma = tabLiczbZparzInd.reduce((a, b) => a + b, 0);
+    return suma / tabLiczbZparzInd.length;
+}
+
+console.log(liczSrNaParzInd(cyfry));
+
+// 9. With a given start value of 0.
+// Iterate the array and add even items and subtract odd ones.
+// [1,6,23,8,4,98,3,7,3,98,4,98]
+
+/** 
+* wykonuje dzialanie: sum(all_even) - sum(all_odd) dla danej tab wejsciowej
+* @param {Array<Number>} liczby - tabela liczb
+* @return {Number} wynik dzialania: sum(all_even) - sum(all_odd)
+*/
+function dodParzOdejmNparz(liczby) {
+    let tabParzyste = liczby.filter((a) => !czyNparz(a));
+    let tabNparzyste = liczby.filter((a) => czyNparz(a));
+    let sumParz = tabParzyste.reduce((a, b) => a + b, 0);
+    let sumNparz = tabNparzyste.reduce((a, b) => a + b, 0);
+    return sumParz - sumNparz;
+}
+
+console.log(dodParzOdejmNparz(cyfry));
