768cb310d7c5ff349889f799651ce4c7be9f3d6b
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2020-11-23 14:51:10 +0100

    kosmetyka + dodano z3a-c z pd4

--- s1z4_21_11_2020/pd4/pd4.js
+++ s1z4_21_11_2020/pd4/pd4.js
@@ -4,7 +4,7 @@
 // Podyplomowka: JavaScript Developer 2020/2021
 // funkcje nie sprawdzaja zalozen, nie lapia bledow (try...catch), itd.
 // funkcje nie sa optymalizowane pod katem szybkosci
-// ver 0.0
+// ver 0.1
 /////////////////
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -47,8 +47,8 @@ console.log(jakasLiczba + " po odwroceniu daje: " + jakasLiczba.reverse());
 //                                 zadanie 3                                 //
 ///////////////////////////////////////////////////////////////////////////////
 // 3) Based on included JSON file.
-//  a. Create a function that will return Json from the file. a
-//  b. Create a structures to hold data from the file. b
+//  a. Create a function that will return Json from the file.
+//  b. Create a structures to hold data from the file
 //  c. Map data from function a to structure from b.
 //  d. Create object that will give us data about:
 //     i. How much money was spend in 2014
@@ -56,6 +56,56 @@ console.log(jakasLiczba + " po odwroceniu daje: " + jakasLiczba.reverse());
 //     iii. What type on transaction was having what spending’s.
 //     iv. Values of the spending in each month
 //     v. Values of the spending in each day of the week
+
+
+//  a. Create a function that will return Json from the file.
+// na NodeJS uzywalismy
+// fs = require('fs');
+// fs.readFile(file, [encoding], [callback]);
+// zakladam, ze uzywamy require(path) jak podano w pliku 1.js z githuba
+
+/**
+ * zwraca JSON wczytany z Pliku
+ * @param {String} sciezka - sciezka do pliku JSON na dysku
+ * @return {Object} JSON wczytany z pliku
+ */
+function getJSONfromFile(sciezka) {
+    return require(sciezka);
+}
+
+
+//  b. Create a structures to hold data from the file
+// tzn.? niejasne, dac let-a/const-a aby trzymac wczytany obiekt?
+const transObj = getJSONfromFile("./Data.json");
+
+//  c. Map data from function a to structure from b.
+// niejasne, np. z jakiej funkcji (mam sobie wymyslic?)
+
+/**
+ * zamienia kwote na PLN-y (zaokragla do 2 miejsc po przecinku i dokleja " PLN")
+ * @param {Number|String} kwota - kwota
+ * @return {String} kwota w PLN-ach (format "123.22 PLN")
+ */
+function numToPLN(kwota) {
+    return parseFloat(kwota).toFixed(2) + " PLN";
+}
+
+/**
+ * mapuje dana funkcje na wszystkie elementy obiektu
+ * @param {Function} fun - funkcja stosowana na kazdym elemencie obiektu
+ * @return {Array} tablica wynkikowa (wartosci zwracane przez funkcje)
+ */
+function myMap(fun, transakcje) {
+    let tabWyn = [];
+    for (let i = 0; i < transakcje.length; i++) {
+        tabWyn.push(fun(transakcje[i]));
+    }
+    return tabWyn;
+}
+
+console.log(myMap((obj) => numToPLN(obj.cost), transObj));
+// hmm, wbud map tez wydaje sie dzialac, chociaz powinno byc wywolane na arrayu
+console.log(transObj.map((obj) => numToPLN(obj.cost)));
 
 
 ///////////////////////////////////////////////////////////////////////////////
