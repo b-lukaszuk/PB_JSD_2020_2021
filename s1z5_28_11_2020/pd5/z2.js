4e34419aa05a5041adab72ca4c044beb6a3ee1ae
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2020-12-09 23:04:12 +0100

    kosmetyka + definicja klasy Kwadrat9x9 i podstaw jej funkcjonalnosci

--- s1z5_28_11_2020/pd5/z2.js
+++ s1z5_28_11_2020/pd5/z2.js
@@ -5,65 +5,112 @@
 // wlasnoreczne (na piechote) rozwiazanie 1 latwego sudoku z
 // https://sudoku.com/easy/
 // i zobaczmy czy to wystarczy
+// (do rozwiazania jest proste sudoku wiec powinno)
 
 // sudoku to duzy kwadrat 9x9 (malych kwadratow)
-// na razie bedzie reprezentowany jako tablica tablic
-// w komentarzach indeksacja Python-owa (od 0 i incl-excl)
-let table = [
-  // [0:3, 0:3]
-  [
-    [7, 0, 4],
-    [8, 2, 0],
-    [0, 0, 9],
-  ],
-  // [0:3, 3:6]
-  [
-    [8, 0, 0],
-    [5, 0, 0],
-    [4, 3, 0],
-  ],
-  // [0:3, 6:9]
-  [
-    [3, 0, 1],
-    [0, 4, 0],
-    [5, 0, 0],
-  ],
-  // [3:6, 0:3]
-  [
-    [3, 1, 0],
-    [0, 8, 0],
-    [9, 0, 7],
-  ],
-  // [3:6, 3:6]
-  [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-  ],
-  // [3:6, 6:9]
-  [
-    [8, 0, 7],
-    [0, 1, 0],
-    [0, 3, 2],
-  ],
-  // [6:9, 0:3]
-  [
-    [0, 0, 6],
-    [0, 7, 0],
-    [5, 0, 8],
-  ],
-  // [6:9, 3:6]
-  [
-    [0, 1, 5],
-    [0, 0, 9],
-    [0, 0, 2],
-  ],
-  // [6:9, 6:9]
-  [
-    [4, 0, 0],
-    [0, 6, 5],
-    [1, 0, 3],
-  ],
+// bedzie reprezentowana jako tablica 81 elt [0:80], bo 9x9
+
+//// kopia - potrzebna mi, kompowi wszystki jedno
+//// daje tutaj, bo prettier-JS mi to rozbija do postaci malo czytelnej
+// [
+//     // [0:3, 0:9]
+//     7, 0, 4,       8, 0, 0,       3, 0, 1,
+//     8, 2, 0,       5, 0, 0,       0, 4, 0,
+//     0, 0, 9,       4, 3, 0,       5, 0, 0,
+
+//     // [3:6, 0:9]
+//     3, 1, 0,       0, 0, 0,       8, 0, 7,
+//     0, 8, 0,       0, 0, 0,       0, 1, 0,
+//     9, 0, 7,       0, 0, 0,       0, 3, 2,
+
+//     // [6:9, 0:9]
+//     0, 0, 6,       0, 1, 5,       4, 0, 0,
+//     0, 7, 0,       0, 0, 9,       0, 6, 5,
+//     5, 0, 8,       0, 0, 2,       1, 0, 3,
+// ];
+
+let tabela9x9 = [
+  7,
+  0,
+  4,
+  8,
+  0,
+  0,
+  3,
+  0,
+  1,
+  8,
+  2,
+  0,
+  5,
+  0,
+  0,
+  0,
+  4,
+  0,
+  0,
+  0,
+  9,
+  4,
+  3,
+  0,
+  5,
+  0,
+  0,
+  3,
+  1,
+  0,
+  0,
+  0,
+  0,
+  8,
+  0,
+  7,
+  0,
+  8,
+  0,
+  0,
+  0,
+  0,
+  0,
+  1,
+  0,
+  9,
+  0,
+  7,
+  0,
+  0,
+  0,
+  0,
+  3,
+  2,
+  0,
+  0,
+  6,
+  0,
+  1,
+  5,
+  4,
+  0,
+  0,
+  0,
+  7,
+  0,
+  0,
+  0,
+  9,
+  0,
+  6,
+  5,
+  5,
+  0,
+  8,
+  0,
+  0,
+  2,
+  1,
+  0,
+  3,
 ];
 
 /**
@@ -132,10 +179,14 @@ function usNumZtab(tab, num) {
  */
 class Kwadr3x3 {
   /**
-   * @param {Array<Object>} tabPol - tablica obiektow Pole1x1
+   * @param {<Object>} pola1x1 - wiele obiektow (do 9), klasy pole
    */
-  constructor(tabPol) {
-    this.tabPol = tabPol;
+  constructor(...pola1x1) {
+    if (pola1x1.length != 9) {
+      console.log("blad! obiektow pole1x1 musi byc dokladnie 9");
+    } else {
+      this.tabPol = pola1x1;
+    }
   }
   /**
    * updateuje kandydatow we wszystkich ob. Pole1x1
@@ -155,3 +206,45 @@ class Kwadr3x3 {
     }
   }
 }
+
+class Kwadrat9x9 {
+  constructor(tab9x9) {
+    this.tabPol9x9 = [];
+    for (let w = 0; w < 9; w++) {
+      for (let k = 0; k < 9; k++) {
+        this.tabPol9x9.push(new Pole1x1(tab9x9[9 * w + k], (w = w), (k = k)));
+      }
+    }
+  }
+  // drukuje sudoku (kwadrat9x9) do konsoli
+  print() {
+    // linia otwierajaca (pierwsza z gory)
+    console.log("-------------------------");
+
+    for (let w = 0; w < 9; w++) {
+      // iteracja wiersze
+      let wierszDoDruku = "| "; // bok najbardziej od lewej
+
+      for (let k = 0; k < 9; k++) {
+        // iteracja kolumny
+        let id = 9 * w + k; // id z tabPol9x9
+        wierszDoDruku += this.tabPol9x9[id].getNum() + " ";
+        // co 3 kolumny prawy bok kwadratu 3x3
+        // +1 bo inaczej indeks od 0 zaburza artymetyke z modulo
+        if ((k + 1) % 3 === 0) {
+          wierszDoDruku += "| ";
+        }
+      }
+      console.log(wierszDoDruku);
+      // co 3 wiersze dol boku kwadratu 3x3
+      // +1 bo indeks od 0 zaburza artymetyke z modulo
+      if ((w + 1) % 3 === 0) {
+        console.log("-------------------------");
+      }
+    }
+  }
+}
+
+kwadrat9x9 = new Kwadrat9x9(tabela9x9);
+// console.log(kwadrat9x9.tabPol9x9);
+kwadrat9x9.print();
