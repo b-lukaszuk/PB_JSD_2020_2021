cc2b0f51714fe153e06e7599ae883f8a8265f9d7
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-01-06 18:28:53 +0100

    pd5, kosmetyka (uproszczenie kodu od josephus problem)

--- s1z5_28_11_2020/pd5/josephus_problem/z1.ts
+++ s1z5_28_11_2020/pd5/josephus_problem/z1.ts
@@ -1,53 +1,47 @@
-import { Person } from './person'
+// tabela osob w jaskini (numerki)
+let people: Array<number> = [];
 
-// lista osob w jaskini
-let people: Array<Person> = [];
-
-// nadanie imion/id osobom w jaskini
+// nadanie imion/id osobom w jaskini (1-7, incl-incl)
 for (let i = 1; i < 8; i++) {
-    people.push(new Person(i));
+    people.push(i);
 }
 
-console.log("liczba osob na poczatku: ");
-console.log(people.map((pers) => pers.getId()));
+console.log("Poczatkowo w jaskini sa: ");
+console.log(people.toString() + "\n");
 
 /**
-* imituje zabicie jednej osoby z jaskini przez druga osobe
-* informacje o zdarzeniu wyswietla na ekranie
-* zmienia INPLACE tabele people
-* zawsze pierwsza osoba na liscie (index 0) 
-* zabija druga osobe na liscie (index 1)
-* po czym sama idzie na koniec kolejki
-*/
+ * imituje zabicie jednej osoby z jaskini przez druga osobe
+ * informacje o zdarzeniu wyswietla na ekranie
+ * zmienia INPLACE tabele people
+ * zawsze pierwsza osoba na liscie (index 0)
+ * zabija druga osobe na liscie (index 1)
+ * po czym sama idzie na koniec kolejki
+ */
 function killPerson(): void {
 
     // komunikat o zabiciu (kto => kogo)
-    console.log(
-	people[0].getId().toString() +
-	" kills " +
-	people[1].getId().toString()
-	);
+    console.log(people[0] + " kills " + people[1]);
 
-    // metoda splice usuwa INPLACE
+    // metoda splice, usuwa INPLACE
     people.splice(1, 1);
 
     // przesuniecie na koniec kolejki
     // splice usuwa INPLACE,
     // zwraca tez usuniety element(y) jako array (chocby 1 elementowy)
-    let persToEndOfLine: Person = people.splice(0, 1)[0];
+    let persToEndOfLine: number = people.splice(0, 1)[0];
     people.push(persToEndOfLine);
 }
 
 /**
-* imituje caly ciag zabic, az zostanie 1 osoba w jaskini
-* kazde zabicie wyswietla informacje (funkcja killPerson())
-* zakonczenie zabijania wyswietla informacje o tym kto przezyl
-* zmienia INPLACE tabele people
-* wykorzystuje rekursje/rekurencje
-*/
+ * imituje caly ciag zabic, az zostanie 1 osoba w jaskini
+ * kazde zabicie wyswietla informacje (funkcja killPerson())
+ * zakonczenie zabijania wyswietla informacje o tym kto przezyl
+ * zmienia INPLACE tabele people
+ * wykorzystuje rekursje/rekurencje
+ */
 function getSurvivor(): void {
     if (people.length === 1) {
-	console.log(people[0].getId().toString() + " remains alive");
+	console.log("\n" + people[0] + " remains alive");
 	// return potrzebne aby kiedys zakonczyc rekurencje
 	return // domyslnie zwraca undefined
     } else {
@@ -56,4 +50,5 @@ function getSurvivor(): void {
     }
 }
 
+console.log("zaczynamy zabijanie do ostatniego zywego\n");
 getSurvivor();
