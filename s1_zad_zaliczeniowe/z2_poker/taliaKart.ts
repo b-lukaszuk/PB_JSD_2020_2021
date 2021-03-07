7e7134e2db1cdd04dbf02c4b528f41c8a94ae73d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-03-07 11:36:07 +0100

    poker, talia kart, kosmetyka

--- s1_zad_zaliczeniowe/z2_poker/taliaKart.ts
+++ s1_zad_zaliczeniowe/z2_poker/taliaKart.ts
@@ -12,11 +12,11 @@ class Talia {
      * tworzy talie 52 kart
      */
     public constructor() {
-	for (let i = 2; i < 15; i++) {
-	    for (let j = 1; j < 5; j++) {
-		this.talia.push(new Karta(i, j));
-	    }
-	}
+        for (let i = 2; i < 15; i++) {
+            for (let j = 1; j < 5; j++) {
+                this.talia.push(new Karta(i, j));
+            }
+        }
     }
 
     /**
@@ -25,11 +25,11 @@ class Talia {
      * @returns {Karta} - obiekt klasy karta (1) z this.talia
      */
     private getRandCard(): Karta {
-	// liczba losowa od 0 (incl) do dl tablicy (excl)
-	let losId: number = Math.floor(Math.random() * this.talia.length);
-	// array.splice usuwa elt(y) o podanym ind
-	// zwraca 1-eltowa tab kart, wiec [0]
-	return this.talia.splice(losId, 1)[0];
+        // liczba losowa od 0 (incl) do dl tablicy (excl)
+        let losId: number = Math.floor(Math.random() * this.talia.length);
+        // array.splice usuwa elt(y) o podanym ind
+        // zwraca 1-eltowa tab kart, wiec [0]
+        return this.talia.splice(losId, 1)[0];
     }
 
     /**
@@ -39,11 +39,17 @@ class Talia {
      * @returns {Array<Karta>} - tablica n kart
      */
     public getNrandCards(n: number): Array<Karta> {
-	let kartyDoZwrotu: Array<Karta> = [];
-	for (let i = 0; i < n; i++) {
-	    kartyDoZwrotu.push(this.getRandCard());
-	}
-	return kartyDoZwrotu;
+        let kartyDoZwrotu: Array<Karta> = [];
+        if (n > this.talia.length) {
+            console.log("There are not enough cards in the deck to deal them");
+            console.log("Teturning empty Array<Karta>");
+        } else {
+
+            for (let i = 0; i < n; i++) {
+                kartyDoZwrotu.push(this.getRandCard());
+            }
+            return kartyDoZwrotu;
+        }
     }
 
     /**
@@ -51,8 +57,8 @@ class Talia {
      * @returns {number} - liczba kart aktualnie w talii
      */
     public getLkart(): number {
-	return this.talia.length;
+        return this.talia.length;
     }
 }
 
-export {Talia}
+export { Talia }
