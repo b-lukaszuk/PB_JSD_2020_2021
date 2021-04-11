4a209bb9a17eb746a5de5b7de17306f3234dc9a9
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-04-11 18:40:32 +0200

    piece, zmiana konstruktora (dod. rowId, colId) + funkcje ruszajace

--- s2z3_10_04_2021/02/chessPieces/piece.ts
+++ s2z3_10_04_2021/02/chessPieces/piece.ts
@@ -6,10 +6,16 @@ class Piece {
     // 5 - rook
     // 6 - queen
     // 7 - king
+    // 8 - taken (possible for other piece to move to)
     private _value: number;
+    private _rowId: number;
+    private _colId: number;
+    protected directions: Array<number> = [12, 2, 3, 5, 6, 7, 9, 10];
 
-    public constructor(value: number) {
+    public constructor(rowId: number, colId: number, value: number) {
         this._value = value;
+        this._rowId = rowId;
+        this._colId = colId;
     }
 
     public getValue() {
@@ -18,6 +24,83 @@ class Piece {
 
     public toStr(): string {
         return "   ";
+    }
+
+    private getNto12(howMany: number): Array<number> {
+        let result: Array<number> = [this._rowId + howMany,
+        this._colId];
+        let isOk: boolean = result.every((a) => { return a >= 0 && a < 8 });
+        return isOk ? result : undefined;
+    }
+
+    private getNto6(howMany: number): Array<number> {
+        let result: Array<number> = [this._rowId - howMany,
+        this._colId];
+        let isOk: boolean = result.every((a) => { return a >= 0 && a < 8 });
+        return isOk ? result : undefined;
+    }
+
+    private getNto3(howMany: number): Array<number> {
+        let result: Array<number> = [this._rowId,
+        this._colId + howMany];
+        let isOk: boolean = result.every((a) => { return a >= 0 && a < 8 });
+        return isOk ? result : undefined;
+    }
+
+    private getNto9(howMany: number): Array<number> {
+        let result: Array<number> = [this._rowId,
+        this._colId - howMany];
+        let isOk: boolean = result.every((a) => { return a >= 0 && a < 8 });
+        return isOk ? result : undefined;
+    }
+
+    private getNto2(howMany: number): Array<number> {
+        let result: Array<number> = [this._rowId + howMany,
+        this._colId + howMany];
+        let isOk: boolean = result.every((a) => { return a >= 0 && a < 8 });
+        return isOk ? result : undefined;
+    }
+
+    private getNto10(howMany: number): Array<number> {
+        let result: Array<number> = [this._rowId + howMany,
+        this._colId - howMany];
+        let isOk: boolean = result.every((a) => { return a >= 0 && a < 8 });
+        return isOk ? result : undefined;
+    }
+
+    private getNto5(howMany: number): Array<number> {
+        let result: Array<number> = [this._rowId - howMany,
+        this._colId + howMany];
+        let isOk: boolean = result.every((a) => { return a >= 0 && a < 8 });
+        return isOk ? result : undefined;
+    }
+
+    private getNto7(howMany: number): Array<number> {
+        let result: Array<number> = [this._rowId - howMany,
+        this._colId - howMany];
+        let isOk: boolean = result.every((a) => { return a >= 0 && a < 8 });
+        return isOk ? result : undefined;
+    }
+
+    protected getMvNto(direction: number, distance: number): Array<number> {
+        switch (direction) {
+            case 12:
+                return this.getNto12(distance);
+            case 2:
+                return this.getNto2(distance);
+            case 3:
+                return this.getNto3(distance);
+            case 5:
+                return this.getNto5(distance);
+            case 6:
+                return this.getNto6(distance);
+            case 7:
+                return this.getNto7(distance);
+            case 9:
+                return this.getNto9(distance);
+            default:
+                return this.getNto10(distance);
+        }
     }
 }
 
