28f387080a95b1fdbbbf408ceac47aa4dc529291
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-04-11 18:42:38 +0200

    krol, zmina konstr. (przyjm. pozycje) + met wypisuj. wszystkie ruchy

--- s2z3_10_04_2021/02/chessPieces/king.ts
+++ s2z3_10_04_2021/02/chessPieces/king.ts
@@ -1,12 +1,25 @@
 import Piece from "./piece";
 
 class King extends Piece {
-    public constructor() {
-        super(7);
+
+    public constructor(rowId: number, colId: number) {
+        super(rowId, colId, 7);
     }
 
     public toStr(): string {
         return " K ";
+    }
+
+
+    public getMoves(): Array<Array<number>> {
+        const distance: number = 1;
+        let result: Array<Array<number>> = [];
+        for (let direction of this.directions) {
+            let move: Array<number> = [];
+            move = this.getMvNto(direction, distance);
+            result.push(move);
+        }
+        return result;
     }
 }
 
