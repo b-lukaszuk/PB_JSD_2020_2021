c83e9fab99f12148f30e3d0509535927a87f0556
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-04-12 23:39:23 +0200

    dod ustaw wart pola, popr drukowania/reprezentacji pola,

--- s2z3_10_04_2021/02/chessBoard/chessField.ts
+++ s2z3_10_04_2021/02/chessBoard/chessField.ts
@@ -9,20 +9,49 @@ import { Color } from "../dataTypes/color";
 
 class ChessField {
 
-    public constructor(private _row: number, private _col: number,
-        private _value: number | Piece, private _color: Color) {
+    /**
+     * _value: 0 (empty field), 1 (move indicator) | Piece (occupied field)
+     */
+    public constructor(private _value: number | Piece,
+        private _color: Color) {
+    }
+
+    private getValAsStr(): string {
+        switch (this._value) {
+            case 0:
+                return this._color === Color.Black ? "#" : " ";
+            case 1:
+                return "^";
+            default:
+                return this._value.toString();
+        }
     }
 
     /**
      * returns string of field contents
      */
     public toString(): string {
-        let theString: string = "   ";
+        let theString: string = " " + this.getValAsStr() + " ";
         if (this._color === Color.Black) {
-            theString = "###";
+            theString = "#" + this.getValAsStr() + "#";
         }
         return theString;
     }
+
+    /**
+     * sets field to 0 (empty), 1 (move indicator) or Piece
+     */
+    public setTo(what: number | Piece): void {
+        this._value = what;
+    }
+
+    /**
+     * returns value of a field
+     */
+    public getVal(): number | Piece {
+        return this._value;
+    }
+
 }
 
 export default ChessField;
