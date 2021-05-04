f4f395616625d3a8425b5414f578e29210a527e4
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-05-04 10:09:26 +0200

    dodano karty i podstawowe funkcjonalnosci z nimi zwiazane

--- s2z4_24_04_2021/01/memory/src/app/app.component.ts
+++ s2z4_24_04_2021/01/memory/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+import singlCardFactory from './card/cardFactory';
+import Card from './card/card';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'memory';
+    public title: string = 'memory';
+    public cardFactory = singlCardFactory.getCardFactoryInstance();
+    public cards: Card[][] = [];
+
+    private initializeCards(): void {
+        // number of cards is always even (two of a kind)
+        let nOfRows: number = this.cardFactory.getNOfCards() / 4;
+        let nOfCols: number = this.cardFactory.getNOfCards() / nOfRows;
+        for (let r = 0; r < nOfRows; r++) {
+            let row: Card[] = [];
+            for (let c = 0; c < nOfCols; c++) {
+                let aCard: Card = this.cardFactory.getRandCard();
+                row.push(aCard);
+            }
+            this.cards.push(row);
+        }
+    }
+
+    ngOnInit() {
+        this.initializeCards();
+    }
 }
---
+++ s2z4_24_04_2021/01/memory/src/app/card/card.ts
@@ -0,0 +1,35 @@
+class Card {
+    private _symbol: string;
+    private _covered: boolean;
+    private _matched: boolean;
+
+    public constructor(symbol: string) {
+        this._symbol = symbol;
+        this._covered = true; // covered/uncovered right now
+        this._matched = false; // is matched with other card of a pair
+    }
+
+    public toString(): string {
+        return this._covered || this._matched ? '' : this._symbol;
+    }
+
+    public toggleCovered(): void {
+        this._covered = !this._covered;
+    }
+
+    public toggleMatched(): void {
+        this._matched = !this._matched;
+    }
+
+    public getStatus(): string {
+        if (this._matched) {
+            return 'matched';
+        } else if (this._covered) {
+            return 'covered';
+        } else {
+            return 'uncovered';
+        }
+    }
+}
+
+export default Card;
