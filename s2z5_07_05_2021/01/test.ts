fed91599b26a1c3738922dbddae04b6b5877c29b
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-05-15 10:04:47 +0200

    remodeling przechowywania polaczen i ich dodawania do slownika

--- s2z5_07_05_2021/01/test.ts
+++ s2z5_07_05_2021/01/test.ts
@@ -1,15 +1,16 @@
-let commands: string[] = [
-    "B 1 2",
-    "B 1 3",
-    "B 10 11",
-    "T 1 3",
-    "T 10 2",
-    "B 11 2",
-    "T 10 3",
-    "T 100 103",
-]
+// let commands: string[] = [
+//     "B 1 2",
+//     "B 1 3",
+//     "B 10 11",
+//     "T 1 3",
+//     "T 10 2",
+//     "B 11 2",
+//     "T 10 3",
+//     "T 100 103",
+// ]
 
-let dictConnections = {};
+// graph of connections
+let graph = {};
 
 function execCommand(command: string) {
     let commandArr: string[] = command.split(" ");
@@ -21,23 +22,26 @@ function execCommand(command: string) {
     }
 }
 
-function makeOneWayConnectionAB(nodeA: string, nodeB: string) {
-    if (dictConnections.hasOwnProperty(nodeA)) {
-        dictConnections[nodeA].push(nodeB)
+function addOneWayConToGraph(nodeA: string, nodeB: string) {
+    console.log("creating direct two-way connection between", nodeA, nodeB);
+    if (graph.hasOwnProperty(nodeA)) {
+        graph[nodeA].push(nodeB)
     } else {
-        dictConnections[nodeA] = [nodeB];
+        graph[nodeA] = [nodeB];
+    }
+    if (!graph.hasOwnProperty(nodeB)) {
+        graph[nodeB] = [];
     }
 }
 
-function makeTwoWayConnectionAB(nodeA: string, nodeB: string): void {
-    console.log("creating direct two-way connection between", nodeA, nodeB);
-    makeOneWayConnectionAB(nodeA, nodeB);
-    makeOneWayConnectionAB(nodeB, nodeA);
-}
+addOneWayConToGraph("you", "alice");
+addOneWayConToGraph("you", "bob");
+addOneWayConToGraph("you", "claire");
+addOneWayConToGraph("bob", "anuj");
+addOneWayConToGraph("bob", "peggy");
+addOneWayConToGraph("alice", "peggy");
+addOneWayConToGraph("claire", "thom");
+addOneWayConToGraph("claire", "jonny");
 
-makeTwoWayConnectionAB("1", "2");
-makeTwoWayConnectionAB("1", "3");
-makeTwoWayConnectionAB("4", "5");
-makeTwoWayConnectionAB("4", "3");
-console.log(dictConnections);
+console.log(graph);
 
