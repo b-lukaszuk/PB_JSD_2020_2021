227dc93b30c998579aaef102d9afd0750267a751
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-05-15 11:55:41 +0200

    pierwsza dzialajaca wersja 1 kierunkowego szukania (do impl two-way)

--- s2z5_07_05_2021/01/test.ts
+++ s2z5_07_05_2021/01/test.ts
@@ -1,29 +1,30 @@
-// let commands: string[] = [
-//     "B 1 2",
-//     "B 1 3",
-//     "B 10 11",
-//     "T 1 3",
-//     "T 10 2",
-//     "B 11 2",
-//     "T 10 3",
-//     "T 100 103",
-// ]
+let commands: string[] = [
+    "B 1 2",
+    "B 1 3",
+    "B 10 11",
+    "T 1 3",
+    "T 10 2",
+    "B 11 2",
+    "T 10 3",
+    "T 100 103",
+]
 
 // graph of connections
 let graph = {};
 
-function execCommand(command: string) {
+function execCommand(command: string): void {
     let commandArr: string[] = command.split(" ");
     if (commandArr[0].toLocaleLowerCase() === "b") {
-        // build nodes
-    }
-    if (commandArr[0].toLocaleLowerCase() === "t") {
-        // test nodes existence
+        addOneWayConToGraph(commandArr[1], commandArr[2]);
+    } else if (commandArr[0].toLocaleLowerCase() === "t") {
+        searchForConBetw(commandArr[1], commandArr[2]);
+    } else {
+        console.log("incorrect input");
     }
 }
 
 function addOneWayConToGraph(nodeA: string, nodeB: string) {
-    console.log("creating direct two-way connection between", nodeA, nodeB);
+    console.log("creating direct one-way connection between", nodeA, nodeB);
     if (graph.hasOwnProperty(nodeA)) {
         graph[nodeA].push(nodeB)
     } else {
@@ -34,38 +35,38 @@ function addOneWayConToGraph(nodeA: string, nodeB: string) {
     }
 }
 
-addOneWayConToGraph("you", "alice");
-addOneWayConToGraph("you", "bob");
-addOneWayConToGraph("you", "claire");
-addOneWayConToGraph("bob", "anuj");
-addOneWayConToGraph("bob", "peggy");
-addOneWayConToGraph("alice", "peggy");
-addOneWayConToGraph("claire", "thom");
-addOneWayConToGraph("claire", "jonny");
-
 // one way search for connection
 function searchForConBetw(start: string, end: string): boolean {
-    let search_queue: string[] = graph[start];
-    let curExamPerson: string = "";
-    let testedPeople: string[] = [];
-    while (search_queue.length !== 0) {
-        // Array.shift() removes firts element of an array
-        curExamPerson = search_queue.shift();
-        if (!testedPeople.includes(curExamPerson)) {
-            if (curExamPerson === end) {
-                console.log("connection between", start, "and", end, "was found");
-                return true;
-            } else {
-                search_queue = search_queue.concat(graph[curExamPerson]);
-                testedPeople.push(curExamPerson);
+    if (graph.hasOwnProperty(start)) {
+        let search_queue: string[] = graph[start];
+        let curExamPerson: string = "";
+        let testedPeople: string[] = [];
+        while (search_queue.length !== 0) {
+            // Array.shift() removes firts element of an array
+            curExamPerson = search_queue.shift();
+            if (!testedPeople.includes(curExamPerson)) {
+                if (curExamPerson === end) {
+                    console.log("connection between", start, "and", end, "was found");
+                    return true;
+                } else {
+                    search_queue = search_queue.concat(graph[curExamPerson]);
+                    testedPeople.push(curExamPerson);
+                }
             }
         }
+        console.log("no connection between", start, "and", end, "was found");
+        return false;
+    } else {
+        console.log("no connection between", start, "and", end, "was found");
+        return false;
     }
-    console.log("no connection between", start, "and", end, "was found");
-    return false
 }
 
-console.log(graph);
-searchForConBetw("you", "thom");
-searchForConBetw("you", "piotrek");
 
+function main() {
+    for (let command of commands) {
+        execCommand(command);
+    }
+}
+
+main();
